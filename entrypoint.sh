6b82759326487d6207fa5f676d5ea4230eabcd0a
Felix Mölder <felix.moelder@uni-due.de>
2023-08-07 15:31:42 +0200

    fix action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,15 @@ if [ "$5" = 'containerize' ] ; then
     snakemake --directory $1 --snakefile $2 --show-failed-logs $3 --containerize > Dockerfile
     # return disk space usage on failing
     if [[ "$?" -ne 0 && "$6" = true ]] ; then
-        du -h -d3 $1
+        du -h -d3 $1 && exit 1
     fi
 elif [ "$5" = 'run' ] ; then
     # run snakemake with given args
     snakemake --directory $1 --snakefile $2 --show-failed-logs $3
     # return disk space usage on failing
     if [[ "$?" -ne 0 && "$6" = true ]] ; then
-        du -h -d3 $1
+        df -h
+        du -h -d3 $1 && exit 1
     fi
 else
     echo "Task input not recognized." && exit 1
