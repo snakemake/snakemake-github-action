fe60326d6321f410b6ae0c4284c392eb0010a7df
Johannes Köster <johannes.koester@tu-dortmund.de>
2023-08-07 20:50:21 +0200

    fix: improve error handling logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,20 +8,19 @@ eval "$4"
 # create container file
 if [ "$5" = 'containerize' ] ; then
     snakemake --directory $1 --snakefile $2 --show-failed-logs $3 --containerize > Dockerfile
-    # return disk space usage on failing
-    if [[ "$?" -ne 0 && "$6" = true ]] ; then
-        du -h -d3 $1 && exit 1
-    fi
 elif [ "$5" = 'run' ] ; then
     # run snakemake with given args
     snakemake --directory $1 --snakefile $2 --show-failed-logs $3
-    # return disk space usage on failing
-    if [[ "$?" -ne 0 && "$6" = true ]] ; then
-        df -h
-        echo "du home\n"
-        du -h -d3 /github/home
-        echo "du working directory\n"
-        du -h -d3 $1 && exit 1
+    if [[ "$?" -ne 0 ]]; then
+        if [[ "$6" = true ]]; then
+            # return disk usage and space on failing
+            df -h
+            echo "du home\n"
+            du -h -d3 /github/home
+            echo "du working directory\n"
+            du -h -d3 $1
+        fi
+        exit 1
     fi
 else
     echo "Task input not recognized." && exit 1
